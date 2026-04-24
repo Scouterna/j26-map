e456241c8d426134351b56f230a0d7c5b9a750c0
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2026-04-24 09:06:03 +0200

    fix: use relative URLs for geojson imports

--- src/common/BaseLayers.tsx
+++ src/common/BaseLayers.tsx
@@ -4,18 +4,18 @@ export function BaseLayers() {
 	return (
 		<>
 			<GeoJsonLayer
-				src="/layers/area_covers.geojson"
+				src="./layers/area_covers.geojson"
 				style={{ color: "transparent", fillColor: "#cdebb0", fillOpacity: 1 }}
 			/>
 			<GeoJsonLayer
-				src="/layers/roads.geojson"
+				src="./layers/roads.geojson"
 				style={{ color: "#b3b3b3", weight: 5, opacity: 1, lineCap: "butt" }}
 				geoScale
 				weightAttribute="width"
 				weightOffset={2}
 			/>
 			<GeoJsonLayer
-				src="/layers/roads.geojson"
+				src="./layers/roads.geojson"
 				style={{ color: "#ffffff", weight: 5, opacity: 1, lineCap: "butt" }}
 				geoScale
 				weightAttribute="width"
